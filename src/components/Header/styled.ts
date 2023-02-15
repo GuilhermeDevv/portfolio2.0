6f4d2e5086f51e8f589e6d7a9067b543dd6a8e36
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-15 01:49:18 -0300

    finalização visual do header

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,5 +1,17 @@
 import styled from 'styled-components';
 
-export const HeaderComponent = styled.header`
-  background-color: red;
+export const Container = styled.header`
+  padding: 5rem 3rem;
+  height: 60px;
+  position: fixed;
+  width: 100vw;
+`;
+export const Content = styled.header`
+  max-height: 100%;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  & div {
+    margin-left: auto;
+  }
 `;
