2c63dab821f1f82e055aa39bdc5e687a525a9397
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-14 23:42:55 -0300

    criação do header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { HeaderComponent } from './styled';
+export function Header() {
+  return (
+    <HeaderComponent>
+      <h1>teste</h1>
+    </HeaderComponent>
+  );
+}
