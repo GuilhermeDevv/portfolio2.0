46df941d28bbafa82d4c43602afcc92ed07683f2
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-17 17:56:32 -0300

    removendo as estilizações do menu do componente header

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -10,6 +10,34 @@ export const Content = styled.div`
 `;
 export const Logo = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  gap: 10px;
+  & span {
+    font-size: 22px;
+  }
+  & span:first-child {
+    background: -webkit-linear-gradient(#eee, #333);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  & span:last-child {
+    color: white;
+  }
+`;
+export const ButtonCv = styled.button`
+  background-color: grey;
+  border-radius: 29px;
+  border: 1px solid grey;
+  color: white;
+  font-size: 15px;
+  padding: 10px 25px;
+  cursor: pointer;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border-color: black;
+    transition: 0.3s;
+  }
 `;
