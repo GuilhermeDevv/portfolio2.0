9e7e142b6ae45d2b5fd2216f95e63b30c800824e
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-20 13:40:22 -0300

    colocando o componente de aboutComponent na pagina about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { AboutComponent } from '../../components/AboutComponent/AboutComponent';
 
 export function About() {
-  return <>teste2</>;
+  return (
+    <>
+      <AboutComponent />
+    </>
+  );
 }
