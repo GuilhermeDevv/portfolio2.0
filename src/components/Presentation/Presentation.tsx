24de11fbe2bae6b95cdd0e86c1a48a1f13f9567d
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-20 14:01:35 -0300

    removendo a imagem, e colocando um componente de imagem

--- src/components/Presentation/Presentation.tsx
+++ src/components/Presentation/Presentation.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import background from '../../assets/background.svg';
-import { Container, Content, ImWhat, Self } from './styles';
+import { Container, Content, ImWhat } from './styles';
+
+import { BackgroundSVG } from '../BackgroundSVG/BackgroundSVG';
 export function Presentation() {
   return (
     <Container>
       <Content>
-        <Self src={background} alt="" />
+        <BackgroundSVG src={background} />
         <ImWhat>
           <h1>Desenvolvedor</h1>
           <span>Fullstack</span>
--- src/components/Presentation/styles.ts
+++ src/components/Presentation/styles.ts
@@ -23,35 +23,7 @@ export const Content = styled.div`
     align-items: center;
   }
 `;
-export const Self = styled.img`
-  width: 550px;
-  position: relative;
-  animation: BackgroundTransition 1s ease-in forwards;
 
-  @keyframes BackgroundTransition {
-    from {
-      opacity: 0;
-      top: -120px;
-    }
-    to {
-      top: 0px;
-    }
-  }
-  @media (max-height: 568px) {
-    top: -40px;
-  }
-  @media (max-width: 960px) {
-    @keyframes BackgroundTransition {
-      0% {
-        opacity: 0;
-        bottom: -120px;
-      }
-      100% {
-        bottom: 0px;
-      }
-    }
-  }
-`;
 export const ImWhat = styled.div`
   position: relative;
   left: -110px;
