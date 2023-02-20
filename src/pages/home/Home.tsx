2d4ed638d30e8d71c24ee62f7139128caf11a4e9
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-20 13:34:00 -0300

    finalizacao estrutural da home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Presentation } from '../../components/Presentation/Presentation';
-import { Layout } from '../../Layout/Layout';
 
 export function Home() {
   return (
     <>
-      <Layout>
-        <Presentation />
-      </Layout>
+      <Presentation />
     </>
   );
 }
