4f364a7fa099ad53e81c5eccce327ef74e431f93
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-23 17:40:42 -0300

    correção na fonte

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -8,7 +8,7 @@ export const GlobalStyle = createGlobalStyle`
   margin: 0;
   box-sizing: border-box;
   list-style: none;
-  font-family: 'Roboto', sans-serif;
+  font-family:  sans-serif;
   color:white;
   font-weight: 500;
   text-decoration-line: none;
