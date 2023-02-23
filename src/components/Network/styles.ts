ace33aadf53a7663b14d5d57532c0024d74264a2
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-23 17:34:17 -0300

    colocando animacao nos icones no load do componente

--- src/components/Network/styles.ts
+++ src/components/Network/styles.ts
@@ -9,22 +9,30 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   gap: 5px;
-  position: relative;
-  animation: NetworkTransition 1.1s cubic-bezier(0.895, 0.03, 0.685, 0.22)
-    forwards;
-  @keyframes NetworkTransition {
-    0% {
-      opacity: 0;
-      bottom: -120px;
-    }
-    100% {
-      bottom: -10px;
-    }
-  }
+
   & span {
     display: flex;
     padding: 1px;
     border-radius: 5px;
+    transition: all 1s linear;
+    opacity: 0;
     background-color: rgba(128, 128, 128, 0.33);
+    animation: NetworkTransition 1s forwards;
+  }
+  & span:nth-child(2) {
+    animation-delay: 0.5s;
+  }
+  & span:nth-child(3) {
+    animation-delay: 1s;
+  }
+  @keyframes NetworkTransition {
+    0% {
+      opacity: 0;
+      transform: translateX(-50px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0px);
+    }
   }
 `;
