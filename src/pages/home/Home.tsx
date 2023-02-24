49fb393aff4467c27258a5abab35d8c62026565a
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-23 23:15:22 -0300

    colocando o menu de redes na home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Network } from '../../components/Network/Network';
 import { Presentation } from '../../components/Presentation/Presentation';
 
 export function Home() {
   return (
     <>
       <Presentation />
+      <Network />
     </>
   );
 }
