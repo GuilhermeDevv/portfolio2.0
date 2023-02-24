d8e6e2493112a5eb2870e3e48f070a760b321594
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-02-23 23:14:21 -0300

    colocandoi a pagina de contatos

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,6 +4,7 @@ import { Home } from '../pages/home/Home';
 import { Layout } from '../Layout/Layout';
 import { About } from '../pages/About/About';
 import { Projects } from '../pages/Projects/Projects';
+import { Contact } from '../pages/Contact/Contact';
 
 export function RoutePage() {
   return (
@@ -12,6 +13,7 @@ export function RoutePage() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
     </Layout>
   );
