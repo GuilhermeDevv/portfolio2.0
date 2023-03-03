e3e6881211a680800af09774d5ed3e7b8f5b4b41
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-03 18:15:10 -0300

    ajuste na estilizacao

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,24 +1,24 @@
 import styled from 'styled-components';
 export const ButtonComponent = styled.button`
   z-index: 1;
-  max-width: 145px;
-  width: 100%;
-  background-color: grey;
+  min-width: 150px;
+
+  background-color: ${({ theme }) => theme.colors.backgroundButton};
+  color: black;
   border-radius: 20px;
-  border: 1px solid grey;
-  color: white;
-  font-size: 15px;
+  box-shadow: 0px 1px 7px 1px ${({ theme }) => theme.colors.backgroundButton};
+  border: 1px solid transparent;
+
   padding: 10px 25px;
   cursor: pointer;
   transition: 0.3s;
 
+  & a {
+    font-size: 16px;
+    color: ${({ theme }) => theme.colors.destaque};
+  }
+
   &:hover {
-    background-color: white;
-    border-color: black;
-    transition: 0.3s;
-    & a {
-      transition: 0.3s;
-      color: black;
-    }
+    transform: scale(1.01);
   }
 `;
