b57f88887292da1202c99f3e314e25e9639ca022
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-04 18:16:04 -0300

    ajuste na estilizacao

--- src/components/BackgroundSVG/styles.ts
+++ src/components/BackgroundSVG/styles.ts
@@ -1,31 +1,44 @@
 import styled from 'styled-components';
-
-export const Self = styled.img`
+interface ImageProps {
+  src: string;
+  alt?: string;
+  animation: string; // Definindo a interface da propriedade "animation"
+}
+export const Self = styled.img<ImageProps>`
   width: 450px;
   position: relative;
-  animation: BackgroundTransition 1s ease-in forwards;
+  animation: ${({ animation }) => animation} 2000ms ease-out;
+  animation-fill-mode: ${({ animation }) =>
+    animation !== 'mexer' ? 'forwards' : ''};
+  animation-iteration-count: ${({ animation }) =>
+    animation === 'mexer' ? 'infinite' : ''};
 
-  @keyframes BackgroundTransition {
-    from {
-      opacity: 0;
-      top: -120px;
+  @keyframes mexer {
+    0% {
+      opacity: 1;
+      transform: translateY(0px);
     }
-    to {
-      top: 0px;
+    50% {
+      opacity: 1;
+      transform: translateY(10px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0px);
     }
   }
   @media (max-height: 568px) {
     top: -40px;
   }
-  @media (max-width: 960px) {
-    @keyframes BackgroundTransition {
-      0% {
-        opacity: 0;
-        bottom: -120px;
-      }
-      100% {
-        bottom: 0px;
-      }
+
+  @keyframes BackgroundTransition {
+    0% {
+      opacity: 0;
+      transform: translateY(120px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0px);
     }
   }
 `;
