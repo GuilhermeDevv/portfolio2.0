d0c86e573360ad7ddd6e9d68b47c9e522f398503
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-03-07 18:03:56 -0300

    inclusao de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { RoutePage } from './routes/Routes';
 import { GlobalStyle } from './styles/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import { Black, Light } from './theme/theme';
+import ToggleTheme from './components/ToggleTheme/ToggleTheme';
+import { useState } from 'react';
 
 export function App() {
+  const [themeCurrent, setTheme] = useState<boolean>(false);
+  function toggleTheme() {
+    setTheme(!themeCurrent);
+  }
+
   return (
     <>
-      <GlobalStyle />
-      <RoutePage />
+      <ThemeProvider theme={themeCurrent ? Light : Black}>
+        <GlobalStyle />
+        <RoutePage />
+        <ToggleTheme toggleThemeFn={toggleTheme} />
+      </ThemeProvider>
     </>
   );
 }
