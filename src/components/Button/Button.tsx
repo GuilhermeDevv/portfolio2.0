9a9608ff384fa82feef204ece00cdec65db17d83
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-06-06 15:37:00 -0300

    including option for button download

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { ButtonComponent } from './styles';
-type Props = { text: string; link: string };
-export function Button({ text, link }: Props) {
+type Props = { text: string; link: string; isDownload?: boolean };
+export function Button({ text, link, isDownload }: Props) {
   return (
     <ButtonComponent>
-      <a href={link}>{text}</a>
+      <a href={link} download={isDownload}>
+        {text}
+      </a>
     </ButtonComponent>
   );
 }
