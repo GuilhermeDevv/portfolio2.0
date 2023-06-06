05894e44ca53b05af6094f57f1717c5002af3480
GuilhermeDevv <ddc3.0cf1@gmail.com>
2023-06-06 15:33:49 -0300

    including curriculum download

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Button } from '../Button/Button';
 import { Container, Content, Logo } from './styled';
-
+import MyCV from '/src/pdf/Guilherme-santos.pdf';
 export function Header() {
   return (
     <Container>
       <Content>
         <Logo>
           <span>Guilherme</span>
-          <span>Santos</span>
+          <span>Correa</span>
         </Logo>
-        <Button link="" text="Download CV" />
+        <Button link={MyCV} text="Download CV" isDownload={true} />
       </Content>
     </Container>
   );
